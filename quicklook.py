7dc5838523379e548c88e068bf8cb65281ef5e8e
bgreene777 <bgreene777@gmail.com>
2020-03-02 16:53:30 -0600

    plot water vapor differences too

--- quicklook.py
+++ quicklook.py
@@ -105,6 +105,7 @@ rc('font',weight='normal',size=20,family='serif',serif='Computer Modern Roman')
 rc('text',usetex='True')
 # AERI only
 # create meshgrid
+print("---Plotting---")
 for d in range(20, 24):
     iz = np.where(AERI_dic[d]["height"] <= 4.)[0]
     X1, Y1 = np.meshgrid(AERI_dic[d]["hour"], AERI_dic[d]["height"][iz])
@@ -169,7 +170,6 @@ for d in range(20, 24):
 # ---------------------------------------------------------- #
 # Plot differences of each time-height relative to AERI Only #
 # ---------------------------------------------------------- #
-print("---Plotting---")
 for d in range(20, 24):
     # grab data
     # heights below 4 km (all files symmetric)
@@ -186,28 +186,45 @@ for d in range(20, 24):
     AERI_T = AERI_dic[d]["temperature"][:, iz]
     AERIrLID_T = AERIrLID_dic[d]["temperature"][:, iz]
     AERIvDIAL_T = AERIvDIAL_dic[d]["temperature"][:, iz]
+    # grab w data
+    AERI_w = AERI_dic[d]["waterVapor"][:, iz]
+    AERIrLID_w = AERIrLID_dic[d]["temperature"][:, iz]
+    AERIvDIAL_w = AERIvDIAL_dic[d]["temperature"][:, iz]
     # store in a dictionary for looping access
     T_dic = {0: AERI_T,
              1: AERIrLID_T,
              2: AERIvDIAL_T}
+    w_dic = {0: AERI_w,
+             1: AERIrLID_w,
+             2: AERIvDIAL_w}
     # interpolate in time at each level
     hrs_interp = hrs_all[ilong]
     T_dic_interp = {}
+    w_dic_interp = {}
     for i in range(3):
         if i == ilong:
             T_dic_interp[i] = T_dic[i]
+            w_dic_interp[i] = w_dic[i]
         else:
             T_new = np.full((len(hrs_interp), len(iz)), np.nan)
+            w_new = np.full((len(hrs_interp), len(iz)), np.nan)
             for j in range(len(iz)):
-                f = interp1d(hrs_all[i], T_dic[i][:, j], fill_value="extrapolate")
-                fnew = f(hrs_interp.data)
-                T_new[:, j] = fnew
+                fT = interp1d(hrs_all[i], T_dic[i][:, j], fill_value="extrapolate")
+                fnewT = fT(hrs_interp.data)
+                T_new[:, j] = fnewT
+                fw = interp1d(hrs_all[i], w_dic[i][:, j], fill_value="extrapolate")
+                fneww = fw(hrs_interp.data)
+                w_new[:, j] = fneww
             T_dic_interp[i] = T_new
+            w_dic_interp[i] = w_new
 
     # calculate differences
     AERI_LID_T_diff = (T_dic_interp[0] - T_dic_interp[1]).transpose()
     AERI_DIAL_T_diff = (T_dic_interp[0] - T_dic_interp[2]).transpose()
+    AERI_LID_w_diff = (w_dic_interp[0] - w_dic_interp[1]).transpose()
+    AERI_DIAL_w_diff = (w_dic_interp[0] - w_dic_interp[2]).transpose()
     # plot
+    # Temperature
     fig2, ax2 = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(16, 8))
     # AERI - AERIrLID
     cfax21 = ax2[0].pcolormesh(hrs_interp, z[iz], AERI_LID_T_diff, 
@@ -242,3 +259,39 @@ for d in range(20, 24):
     print(f"Saving figure: {fsave2}")
     fig2.savefig(os.path.join(figpath, fsave2), dpi=150, fmt="pdf")
     plt.close(fig2)
+
+    # Water Vapor
+    fig3, ax3 = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(16, 8))
+    # AERI - AERIrLID
+    cfax31 = ax3[0].pcolormesh(hrs_interp, z[iz], AERI_LID_w_diff, 
+        cmap=cmocean.cm.balance,vmin=-10,vmax=10)
+    cbar31 = plt.colorbar(cfax31, ax=ax3[0])
+    cbar31.ax.set_ylabel("$\Delta w$ [g Kg$^{-1}$]")
+    ax3[0].set_ylabel("Altitude [km AGL]")
+    ax3[0].set_ylim([0, 4])
+    ax3[0].yaxis.set_major_locator(MultipleLocator(1))
+    ax3[0].yaxis.set_minor_locator(MultipleLocator(0.25))
+    ax3[0].set_title(f"2017-05-{d} AERI - Raman")
+    cfax31.set_edgecolor("face")
+
+    # AERI - AERIvDIAL
+    cfax32 = ax3[1].pcolormesh(hrs_interp, z[iz], AERI_DIAL_w_diff,
+        cmap=cmocean.cm.balance,vmin=-10,vmax=10)
+    cbar32 = plt.colorbar(cfax32, ax=ax3[1])
+    cbar32.ax.set_ylabel("$\Delta w$ [g Kg$^{-1}$]")
+    ax3[1].set_xlabel("Hour [UTC]")
+    ax3[1].set_ylabel("Altitude [km AGL]")
+    ax3[1].set_xlim([0, 24])
+    ax3[1].set_ylim([0, 4])
+    ax3[1].xaxis.set_major_locator(MultipleLocator(3))
+    ax3[1].xaxis.set_minor_locator(MultipleLocator(1))
+    ax3[1].yaxis.set_major_locator(MultipleLocator(1))
+    ax3[1].yaxis.set_minor_locator(MultipleLocator(0.25))
+    ax3[1].set_title("AERI - vDIAL")
+    cfax32.set_edgecolor("face")
+
+    fig3.tight_layout()
+    fsave3 = f"201705{d}_waterVapor_diff.pdf"
+    print(f"Saving figure: {fsave3}")
+    fig3.savefig(os.path.join(figpath, fsave3), dpi=150, fmt="pdf")
+    plt.close(fig3)
